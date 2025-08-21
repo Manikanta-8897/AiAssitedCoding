5a460c35772900e65e0844ff6216065baa447fd8
Manikanta-8897 <2403a51290@sru.edu.in>
2025-08-21 16:43:32 +0530

    Add files via upload

---
+++ ascii.py
@@ -0,0 +1,17 @@
+# char_a='A'
+# ascii_a=ord(char_a)
+# print(ascii_a)
+# a=66
+# ascii_a=chr(a)
+# print(ascii_a)
+# a=15
+# b=20
+# c=a!=b
+# print(c)
+# a=True
+# b=False
+# c=a or b
+# print(c)
+a=False
+b=not a
+print(b)
